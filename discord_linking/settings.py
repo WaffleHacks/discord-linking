43de297d16a73ab35e2c1c0e095af33a89ef03dc
Alex Krantz <alex@krantz.dev>
2022-05-21 00:37:23 -0700

    Add database configuration

--- discord_linking/settings.py
+++ discord_linking/settings.py
@@ -1,8 +1,11 @@
-from os import environ
+from binascii import hexlify
+from os import environ, urandom
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# Set the debug mode
 raw_debug = (environ.get("DEBUG") or "no").lower()
 DEBUG = (
     raw_debug == "yes"
@@ -11,3 +14,9 @@ DEBUG = (
     or raw_debug == "t"
     or raw_debug == "1"
 )
+
+# Cookie signing key
+SECRET_KEY = environ.get("SECRET_KEY") or hexlify(urandom(32)).decode()
+
+# Database configuration
+SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
