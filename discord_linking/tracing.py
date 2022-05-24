dbb17a805303f3544127f33a374587ab0551ae1e
Alex Krantz <alex@krantz.dev>
2022-05-23 21:09:29 -0700

    Instrument jinja2

--- discord_linking/tracing.py
+++ discord_linking/tracing.py
@@ -4,6 +4,7 @@ from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.botocore import BotocoreInstrumentor
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.jinja2 import Jinja2Instrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
 from opentelemetry.sdk.trace import TracerProvider
@@ -22,6 +23,7 @@ def init(app, db):
     if environ.get("OTEL_SERVICE_NAME") is not None:
         BotocoreInstrumentor().instrument()
         FlaskInstrumentor().instrument_app(app)
+        Jinja2Instrumentor().instrument()
         RequestsInstrumentor().instrument()
 
         with app.app_context():
